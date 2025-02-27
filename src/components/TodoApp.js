22ecf440fadf1f130410b5b2258e762254edece4
Jun-04 <95523195+Jun-04@users.noreply.github.com>
2025-02-27 16:51:48 +1100

    Update TodoApp.js

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -7,7 +7,7 @@ import {v4 as uuidv4} from 'uuid';
 class TodoApp extends React.Component {
     state = {
         todos: [
-        id: uuidv4(),
+        {id: uuidv4(),
         title: "Down Picking BPM 50 ~ 80",
         completed: false
         },
